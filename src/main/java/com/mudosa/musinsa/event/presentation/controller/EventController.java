cc6694598106d4006a38bb2c768175df3512b5ee
molcham <molcham@molchamui-MacBookPro.local>
2025-10-31 15:54:34 +0900

    fix: eventList 반환 로직 수정

--- src/main/java/com/mudosa/musinsa/event/presentation/controller/EventController.java
+++ src/main/java/com/mudosa/musinsa/event/presentation/controller/EventController.java
@@ -6,15 +6,14 @@ import com.mudosa.musinsa.event.service.EventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api/events")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 @Slf4j
 
