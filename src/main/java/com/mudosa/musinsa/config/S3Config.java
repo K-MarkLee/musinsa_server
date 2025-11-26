a184cb06ac678f13e4ad6f91b78b7b4d289e5b1e
mj <nampink2300@gmail.com>
2025-11-26 14:30:53 +0900

    FDBD-90 ✨feature[file]: s3를 위한 config 구현

---
+++ src/main/java/com/mudosa/musinsa/config/S3Config.java
@@ -0,0 +1,32 @@
+package com.mudosa.musinsa.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class S3Config {
+
+  @Value("${aws.s3.accessKey}")
+  private String accessKey;
+
+  @Value("${aws.s3.secretKey}")
+  private String secretKey;
+
+  @Bean
+  public AmazonS3 amazonS3Client() {
+    BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+
+    return AmazonS3ClientBuilder.standard()
+        .withRegion(Regions.AP_SOUTHEAST_2)
+        .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+        .build();
+  }
+}
