85d4a17c4ce013a0c586d8b342a778ee3497e563
K-MarkLee <seungyeol6825@gmail.com>
2025-11-27 14:02:37 +0900

    FDBD-80 feat[product] : 상품 커맨드 컨트롤러 테스트 추가 및 권한 추가.

--- src/test/java/com/mudosa/musinsa/product/presentation/controller/ControllerTestSupport.java
+++ src/test/java/com/mudosa/musinsa/product/presentation/controller/ControllerTestSupport.java
@@ -9,12 +9,16 @@ import com.mudosa.musinsa.settlement.domain.repository.SettlementMonthlyMapper;
 import com.mudosa.musinsa.settlement.domain.repository.SettlementPerTransactionMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 // product 컨트롤러 공통 WebMvc 테스트 설정
 @WebMvcTest(controllers = {CartController.class, CategoryQueryController.class, ProductQueryController.class, 
         ProductCommandController.class})
+@Import(ControllerTestSupport.TestMethodSecurityConfig.class)
 public abstract class ControllerTestSupport {
 
     @Autowired
@@ -38,4 +42,9 @@ public abstract class ControllerTestSupport {
     protected SettlementMonthlyMapper settlementMonthlyMapper;
     @MockitoBean
     protected SettlementPerTransactionMapper settlementPerTransactionMapper;
+
+    @Configuration
+    @EnableMethodSecurity
+    static class TestMethodSecurityConfig {
+    }
 }
