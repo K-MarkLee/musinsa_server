49b515c3be52526d9a640457e5392615ef54459f
mj <nampink2300@gmail.com>
2025-12-02 10:38:27 +0900

    FDBD-92 ✨feature[chat]: Multipart -> TempUploadedFile

--- src/main/java/com/mudosa/musinsa/domain/chat/file/AbstractS3Store.java
+++ src/main/java/com/mudosa/musinsa/domain/chat/file/AbstractS3Store.java
@@ -1,5 +1,6 @@
 package com.mudosa.musinsa.domain.chat.file;
 
+import com.mudosa.musinsa.domain.chat.event.TempUploadedFile;
 import io.micrometer.tracing.Tracer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,8 +19,8 @@ public abstract class AbstractS3Store {
   protected String bucketName;
 
   // 1. 채팅 메시지용 키 생성
-  protected String generateMessageKey(Long chatId, Long messageId, MultipartFile file) {
-    String original = Objects.requireNonNullElse(file.getOriginalFilename(), "unknown");
+  protected String generateMessageKey(Long chatId, Long messageId, TempUploadedFile file) {
+    String original = Objects.requireNonNullElse(file.originalFilename(), "unknown");
     String safeName = UUID.randomUUID() + "_" + StringUtils.cleanPath(original);
 
     // chat/{chatId}/message/{messageId}/{uuid_filename}
